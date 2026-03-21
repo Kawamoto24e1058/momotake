325a5803ad83200804043c55f5da923ed61341b2
haru2514 <kharu0414@icloud.com>
2026-03-21 14:58:07 +0900

    決済エラー

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -40,7 +40,6 @@
 declare module '$env/static/private' {
 	export const FIREBASE_SERVICE_ACCOUNT_KEY: string;
 	export const STRIPE_SECRET_KEY: string;
-	export const ANTIGRAVITY_AGENT: string;
 	export const TERM_PROGRAM: string;
 	export const NODE: string;
 	export const INIT_CWD: string;
@@ -64,7 +63,6 @@ declare module '$env/static/private' {
 	export const VSCODE_PROFILE_INITIALIZED: string;
 	export const __CF_USER_TEXT_ENCODING: string;
 	export const npm_execpath: string;
-	export const PAGER: string;
 	export const PATH: string;
 	export const npm_package_json: string;
 	export const _: string;
@@ -147,6 +145,7 @@ declare module '$env/static/public' {
 	export const PUBLIC_FIREBASE_PROJECT_ID: string;
 	export const PUBLIC_FIREBASE_STORAGE_BUCKET: string;
 	export const PUBLIC_STRIPE_CONNECT_ACCOUNT_ID: string;
+	export const PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
 }
 
 /**
@@ -198,7 +197,6 @@ declare module '$env/dynamic/private' {
 	export const env: {
 		FIREBASE_SERVICE_ACCOUNT_KEY: string;
 		STRIPE_SECRET_KEY: string;
-		ANTIGRAVITY_AGENT: string;
 		TERM_PROGRAM: string;
 		NODE: string;
 		INIT_CWD: string;
@@ -222,7 +220,6 @@ declare module '$env/dynamic/private' {
 		VSCODE_PROFILE_INITIALIZED: string;
 		__CF_USER_TEXT_ENCODING: string;
 		npm_execpath: string;
-		PAGER: string;
 		PATH: string;
 		npm_package_json: string;
 		_: string;
@@ -324,6 +321,7 @@ declare module '$env/dynamic/public' {
 		PUBLIC_FIREBASE_PROJECT_ID: string;
 		PUBLIC_FIREBASE_STORAGE_BUCKET: string;
 		PUBLIC_STRIPE_CONNECT_ACCOUNT_ID: string;
+		PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
 		[key: `PUBLIC_${string}`]: string | undefined;
 	}
 }
